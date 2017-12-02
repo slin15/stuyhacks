61d415599ff0785a10bb858961899fc7834e98dc
apple <apple@xadmins-MacBook-Air.local>
2017-12-02 15:04:36 -0500

    added Quiz.java, has everything almost everything

--- Quiz.java
+++ Quiz.java
@@ -1,12 +1,50 @@
 import java.util.Scanner;
-public class Quiz{
+import java.io.*;
+import java.util.*;
 
-    public static void main(String[] args){
-	Scanner sc = new Scanner(System.in);
+public class Quiz {
+        static InputStreamReader isr = new InputStreamReader( System.in );
+    static BufferedReader in = new BufferedReader( isr );
+    
+    public static void main(String [] args) {
 
-	while (sc.hasNext()) {
-	    String line = sc.nextLine();
-	    System.out.println(line);
-	}
+        // The name of the file to open.
+        String fileName = "quiz.txt";
+
+        // This will reference one line at a time
+        String line = null;
+
+        try {
+            // FileReader reads text files in the default encoding.
+            FileReader fileReader = 
+                new FileReader(fileName);
+
+            // Always wrap FileReader in BufferedReader.
+            BufferedReader bufferedReader = 
+                new BufferedReader(fileReader);
+
+            while((line = bufferedReader.readLine()) != null) {
+                System.out.println(line);
+		String [] answers = new String[10];
+		    int counter = 0;
+		    answers[counter] = in.readLine();
+		    counter += 1;
+            }   
+
+            // Always close files.
+            bufferedReader.close();         
+        }
+        catch(FileNotFoundException ex) {
+            System.out.println(
+                "Unable to open file '" + 
+                fileName + "'");                
+        }
+        catch(IOException ex) {
+            System.out.println(
+                "Error reading file '" 
+                + fileName + "'");                  
+            // Or we could just do this: 
+            // ex.printStackTrace();
+        }
     }
 }
